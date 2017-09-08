a8a444b13fbee6ac581a8d963f458c421a3f4d66
Roderick Burkhardt <rburkhardt@gmail.com>
2017-09-07 22:19:45 -0700

    Added team class

--- frontend/src/app/core/services/team.service.ts
+++ frontend/src/app/core/services/team.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {Serializable} from "selenium-webdriver";
 
 @Injectable()
 export class TeamService {
@@ -7,6 +8,27 @@ export class TeamService {
 
 }
 
-export class Team {
+export class Team implements Serializable<Team> {
 
+  id = "";
+  name = "";
+  numOfPlayers = "";
+  totalTeamRank = "";
+  teamType = "";
+
+  serialize(): string {
+    return JSON.stringify(this);
+  }
+
+  deserialize(json) {
+    this.id = json.id;
+    this.name = json.name;
+    this.numOfPlayers = json.numOfPlayers;
+    this.totalTeamRank = json.totalTeamRank;
+    this.teamType = json.teamType;
+  }
+
+  clone(): Team {
+    return new Team().deserialize(JSON.parse(JSON.stringify(this)));
+  }
 }
