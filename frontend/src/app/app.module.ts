49ab1b5488a822f698ecf98d4c509e7e31af4f6d
Roderick Burkhardt <rburkhardt@gmail.com>
2017-10-13 17:37:29 -0700

    Adding pages for displaying the content of the app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,9 @@ import { AppComponent } from './app.component';
 import { ResolveAssetPathPipe } from './core/pipes/resolve-asset-path.pipe';
 
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
-// import { PlayerComponent } from './player/player.component';
 import { CoreModule } from './core/core.module';
-// import { LoginComponent } from './shared/login/login.component';
-// import { SignupComponent } from './shared/create-user/create-user.component';
-// import { ProfileComponent } from './shared/create-profile/create-profile.component';
 import { TBadminModule } from './shared/tbadmin.module';
+import {PagesModule} from './pages/pages.module';
 
 // Application wide providers
 const APP_PROVIDERS = [
@@ -20,16 +17,12 @@ const APP_PROVIDERS = [
 @NgModule({
   declarations: [
     AppComponent,
-    // PlayerComponent,
-    // LoginComponent,
-    // SignupComponent,
-    // ProfileComponent,
   ],
   imports: [
     BrowserModule,
-    // MenuModule,
     CoreModule,
     TBadminModule,
+    PagesModule
   ],
   providers: [
     APP_PROVIDERS,
--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,19 +1,21 @@
 import { Routes, RouterModule } from '@angular/router';
-// import {ModuleWithProviders} from "@angular/core/src/core";
+import {ModuleWithProviders} from '@angular/core/src/core';
+import {PagesComponent} from './pages/pages.component';
 
-export const routes: Routes = [
-  {
-    path: "",
-    loadChildren: "app/"
-  },
-  {
-    path: "player",
-    loadChildren: "app/player/player.module#PlayerModule"
-  },
-  {
-    path: "organization",
-    loadChildren: "app/organization/organization.module#OrganizationModule"
-  }
-];
+// export const routes: Routes = [
+  // {
+    // path: "",
+    // component: PagesComponent
+    // loadChildren: 'app/pages/pages.module#PagesModule'
+  // }
+  // {
+  //   path: "player",
+  //   loadChildren: "app/player/player.module#PlayerModule"
+  // },
+  // {
+  //   path: "organization",
+  //   loadChildren: "app/organization/organization.module#OrganizationModule"
+  // }
+// ];
 
 // export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {useHash: true});
