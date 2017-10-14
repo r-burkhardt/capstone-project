b2c04afb51bdd4c89abe4bd4cef513e6eec86b64
Roderick Burkhardt <rburkhardt@gmail.com>
2017-10-13 17:57:04 -0700

    Updated Favicon, setup roting for pages so that it would redirect to the home component

---
+++ frontend/src/app/pages/pages.routing.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { HomeComponent } from './home/home.component';
+import {OrganizationComponent} from './organization/organization.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: PagesComponent,
+    children: [
+      { path: "", redirectTo: 'home', pathMatch: 'full'},
+      { path: "organization", component: OrganizationComponent }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class PagesRoutingModule {}
