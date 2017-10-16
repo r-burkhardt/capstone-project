5f5ab42fb5faf0e72af3267bb90df4dea8c8ef82
Roderick Burkhardt <rburkhardt@gmail.com>
2017-10-15 20:03:39 -0700

    Setup player profile view

--- frontend/src/app/pages/players/players.component.ts
+++ frontend/src/app/pages/players/players.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Player} from "../../core/services/player.service";
 
 @Component({
   selector: 'app-players',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
+  testPlayer: Player;
+
   constructor() { }
 
   ngOnInit() {
+    this.testPlayer.id = "1234567";
+    this.testPlayer.firstName = "John";
+    this.testPlayer.lastName = "Smith";
+    this.testPlayer.totalRank = "19";
+    this.testPlayer.birthday = "1974-08-02";
+    this.testPlayer.injuries = "left knee";
+    this.testPlayer.yearsPlay = "8";
+    this.testPlayer.heightFeet = "5";
+    this.testPlayer.heightInch = "8";
+    this.testPlayer.zipcode = "95008";
   }
 
 }
