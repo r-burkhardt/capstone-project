f4a6e1f67382b9ae620dd67418ffa85607a91fbb
Roderick Burkhardt <rburkhardt@gmail.com>
2017-10-17 12:22:10 -0700

    Arrange the layout or organization page

--- frontend/src/app/pages/organizations/organizations.module.ts
+++ frontend/src/app/pages/organizations/organizations.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrganizationsComponent } from './organizations.component';
-import {OrganizationsProfileModule} from "./organizations-profile/organizations-profile.module";
+import {OrganizationsProfileModule} from './organizations-profile/organizations-profile.module';
 
 @NgModule({
   imports: [
