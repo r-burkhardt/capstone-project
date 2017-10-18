b4c757da87473deafe86f4ede2d87df13c2cff86
Roderick Burkhardt <rburkhardt@gmail.com>
2017-10-18 07:57:47 -0700

    Continuing work on the organization out

---
+++ frontend/src/app/pages/organizations/org-search/org-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-org-search',
+  templateUrl: './org-search.component.html',
+  styleUrls: ['./org-search.component.css']
+})
+export class OrgSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/src/app/pages/organizations/organizations.module.ts
+++ frontend/src/app/pages/organizations/organizations.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { OrganizationsComponent } from './organizations.component';
 import { OrgProfileModule } from './org-profile/org-profile.module';
 import { OrgMiniDetailModule } from './org-mini-detail/org-mini-detail.module';
-import { OrgInfoDetailModule } from "./org-info-detail/org-info-detail.module";
+import { OrgInfoDetailModule } from './org-info-detail/org-info-detail.module';
+import {OrgSearchModule} from './org-search/org-search.module';
 
 @NgModule({
   imports: [
     CommonModule,
     OrgInfoDetailModule,
     OrgMiniDetailModule,
+    OrgSearchModule,
     OrgProfileModule
   ],
   declarations: [ OrganizationsComponent ],
