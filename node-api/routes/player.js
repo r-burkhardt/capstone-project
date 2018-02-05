77b36808dcc05b10293e69101292dcca2abdf16b
Roderick Burkhardt <rburkhardt@gmail.com>
2018-02-04 22:25:07 -0800

    Adding api to retrieve players that are checked into an event hosted by an organization

--- node-api/routes/player.js
+++ node-api/routes/player.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
     var playerController = require('../controllers/player');
 
     app.get('/services/v1/player', playerController.getAllPlayer);
+    // app.get('/service/v1/player/:orgId/:eventId'); // get players checked into event with organization
     app.get('/services/v1/player/:id', playerController.getPlayer);
     app.get('/services/v1/player/near/:lat/:long/:distance', playerController.getPlayerNearBy);
     app.post('/services/v1/player', playerController.createPlayer);
